dd4d6da48298208257d96f88a389de4796669e99
peleg122 <peleg122@gmail.com>
2019-04-09 15:02:40 +0300

    Update PhysicalNumberTest.cpp

--- PhysicalNumberTest.cpp
+++ PhysicalNumberTest.cpp
@@ -13,51 +13,69 @@ using ariel::PhysicalNumber, ariel::Unit;
 #include "badkan.hpp"
 
 int main() {
-  badkan::TestCase testcase;
-  int grade=0;
-  int signal = setjmp(badkan::longjmp_buffer);
-  if (signal == 0) {
+    badkan::TestCase testcase;
+    int grade=0;
+    int signal = setjmp(badkan::longjmp_buffer);
+    if (signal == 0) {
 
-    // BASIC TESTS - DO NOT CHANGE
-    PhysicalNumber a(2, Unit::KM);
-    PhysicalNumber b(300, Unit::M);
-    PhysicalNumber c(2, Unit::HOUR);
-    PhysicalNumber d(30, Unit::MIN);
+        // BASIC TESTS - DO NOT CHANGE
+        PhysicalNumber a(2, Unit::KM);
+        PhysicalNumber b(300, Unit::M);
+        PhysicalNumber c(2, Unit::HOUR);
+        PhysicalNumber d(30, Unit::MIN);
+        PhysicalNumber e(2, Unit::TON);
+        PhysicalNumber f(500, Unit::KG);
 
-    testcase
-    .setname("Basic output")
-    .CHECK_OUTPUT(a, "2[km]")
-    .CHECK_OUTPUT(b, "300[m]")
+        testcase
+                .setname("Basic output")
+                .CHECK_OUTPUT(a, "2[km]")
+                .CHECK_OUTPUT(b, "300[m]")
 
-    .setname("Compatible dimensions")
-    .CHECK_OUTPUT(b+a, "2300[m]")
-    .CHECK_OUTPUT((a+=b), "2.3[km]")
-    .CHECK_OUTPUT(a, "2.3[km]")
-    .CHECK_OUTPUT(a+a, "4.6[km]")
-    .CHECK_OUTPUT(b-b, "0[m]")
-    .CHECK_OUTPUT(c, "2[hour]")
-    .CHECK_OUTPUT(d, "30[min]")
-    .CHECK_OUTPUT(d+c, "150[min]")
+                .setname("Compatible dimensions")
+                .CHECK_OUTPUT(b+a, "2300[m]")
+                .CHECK_OUTPUT((a+=b), "2.3[km]")
+                .CHECK_OUTPUT(a, "2.3[km]")
+                .CHECK_OUTPUT(a+a, "4.6[km]")
+                .CHECK_OUTPUT(b-b, "0[m]")
+                .CHECK_OUTPUT(c, "2[hour]")
+                .CHECK_OUTPUT(d, "30[min]")
+                .CHECK_OUTPUT(d+c, "150[min]")
 
-    .setname("Incompatible dimensions")
-    .CHECK_THROWS(a+c)
-    .CHECK_THROWS(a+d)
-    .CHECK_THROWS(b+c)
-    .CHECK_THROWS(b+d)
+                .setname("Incompatible dimensions")
+                .CHECK_THROWS(a+c)
+                .CHECK_THROWS(a+d)
+                .CHECK_THROWS(b+c)
+                .CHECK_THROWS(b+d)
 
-    .setname("Basic input")
-    .CHECK_OK(istringstream("700[kg]") >> a)
-    .CHECK_OUTPUT((a += PhysicalNumber(1, Unit::TON)), "1700[kg]")
+                .setname("Basic input")
+                .CHECK_OK(istringstream("700[kg]") >> a)
+                .CHECK_OUTPUT((a += PhysicalNumber(1, Unit::TON)), "1700[kg]")
 
-    // YOUR TESTS - INSERT AS MANY AS YOU WANT
+                .setname("Pelegs Chekings")
+                .CHECK_OUTPUT(e, "2[ton]")
+                .CHECK_OUTPUT(f, "500[kg]")
+                .CHECK_OUTPUT((e+f), "2.5[ton]")
+                .CHECK_OUTPUT((f+e), "2500[kg]")
+                .CHECK_OUTPUT(e-e, "0[ton]")
+                .CHECK_OUTPUT(f-e, "-1500[kg]")
+                .CHECK_OUTPUT(e-e-f, "-0.5[ton]")
+                .CHECK_OUTPUT(e, "2[ton]")
+                .CHECK_OUTPUT(e+e+e+e+e+e+e, "14[ton]")
+                .CHECK_OK(e.SetUnit(Unit::CM))//might be weird check but ...
+                .CHECK_OUTPUT(e, "2[cm]")
+                .CHECK_THROWS(e+f)//no change made to original
 
-      .setname("...")
 
-      .print(cout, /*show_grade=*/false);
-      grade = testcase.grade();
+
+                        // YOUR TESTS - INSERT AS MANY AS YOU WANT
+
+                .setname("...")
+
+                .print(cout, /*show_grade=*/false);
+        grade = testcase.grade();
     } else {
-      testcase.print_signal(signal);
-      grade = 0;
+        testcase.print_signal(signal);
+        grade = 0;
     }
     cout <<  "*** Grade: " << grade << " ***" << endl;
     return grade;
